dbbc77d97b55e13ab245c3e3c5ccc6147cbb38bd
Kyochu <xuzhongiop@qq.com>
2019-12-25 22:26:54 +0800

    Commit For: Piano keys shown on the left.The implementation of the function is all in ShowTheNumberLeftView.java Timestamp 10:26 AM New York Time.

---
+++ app/src/main/java/com/kyochu/musicapp/view/ShowTheNumberLeftView.java
@@ -0,0 +1,63 @@
+package com.kyochu.musicapp.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import org.w3c.dom.Text;
+
+import androidx.annotation.Nullable;
+
+public class ShowTheNumberLeftView extends View {
+    public ShowTheNumberLeftView(Context context) {
+        this(context,null);
+    }
+
+    public ShowTheNumberLeftView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public ShowTheNumberLeftView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+    private Paint paint;
+    private TextPaint textPaint;
+    private void init(){
+        paint=new Paint();
+        paint.setColor(0xff000000);
+        paint.setStrokeWidth(1);
+
+        textPaint=new TextPaint();
+        textPaint.setColor(0xff000000);
+        textPaint.setTextSize(20);
+    }
+
+    private int rectHeight=16;
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width=this.getWidth();
+        int height=this.getHeight();
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        for (int i=0;i<8;i++){
+            canvas.drawLine(0,height/7*i,width,height/7*i,paint);
+            canvas.rotate(-90,width/10*7,height/7*i-height/14+5);
+            canvas.drawText(""+i,width/10*7,height/7*i-height/14+5,textPaint);
+            canvas.rotate(90,width/10*7,height/7*i-height/14+5);
+        }
+        for (int i=0;i<8;i++){
+            if(i!=0&&i!=0&&i!=4&&i!=7) {
+                canvas.drawRect(0, height / 7 * i - rectHeight, width / 5 * 2, height / 7 * i + rectHeight, paint);
+            }
+        }
+
+
+        //draw rect
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawLine(width-1, 0,width-1,height-5, paint);
+    }
+}
